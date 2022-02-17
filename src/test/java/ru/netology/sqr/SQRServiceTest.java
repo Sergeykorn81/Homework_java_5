1c40cd0003209601f1b33b63c6b125bcadfbdbfd
sergeykorn81 <sergeykorn81@gmail.com>
2022-02-17 20:24:21 +0300

    Changed commit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,10 +1,12 @@
 package ru.netology.sqr;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void shouldCountingNumbersOfSquaresInTheLimitOfDiapason() {
         SQRService service = new SQRService();
         int expected = 3;
@@ -14,12 +16,52 @@ class SQRServiceTest {
         assertEquals(expected, actual);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void shouldCountingNumbersOfSquaresInTheLimitOfDiapason1() {
         SQRService service = new SQRService();
         int expected = 8;
 
         int actual = service.countingNumbersOfSquares(100, 300);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountingNumbersOfSquaresNotInDiapason() {
+        SQRService service = new SQRService();
+        int expected = 7;
+
+        int actual = service.countingNumbersOfSquares(100, 300);
+
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountingNumbersOfSquaresInTheLimitOfDiapason2() {
+        SQRService service = new SQRService();
+        int expected = 1;
+
+        int actual = service.countingNumbersOfSquares(100, 101);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountingNumbersOfSquaresInTheLimitOfDiapason3() {
+        SQRService service = new SQRService();
+        int expected = 0;
+
+        int actual = service.countingNumbersOfSquares(0, 99);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountingNumbersOfSquaresInTheLimitOfDiapason4() {
+        SQRService service = new SQRService();
+        int expected = 0;
+
+        int actual = service.countingNumbersOfSquares(300, 300);
 
         assertEquals(expected, actual);
     }
